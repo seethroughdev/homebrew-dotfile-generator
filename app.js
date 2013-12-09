12b322d504ff6a6b2b07e8b91f4241616830582d
Adam L <github@adamin.net>
2013-12-08 23:54:16 -0800

    Added better comments

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var _               = require('lodash'),
     getRemoteFiles  = require('./helpers/get-remote-files'),
 
     // params
-    optPath       = '/opt/homebrew-cask/Caskroom/',
+    optPath         = '/opt/homebrew-cask/Caskroom/',
     appPath         = '/Applications',
     installAppDir   = getArgv(process.argv),
 
@@ -29,6 +29,7 @@ var getAppFiles     = getLocalFiles(appPath);
 var gatherLocalApps = Q.all([getOptFiles, getAppFiles]);
 var getCommonFiles  = Q.all([getGithubFiles, gatherLocalApps]);
 
+
 // get all local applications from Applications and /opt/
 getOptFiles.then(function(files) {
   optFiles = files;
@@ -38,10 +39,12 @@ getAppFiles.then(function(files) {
   appFiles = files;
 }).fin();
 
+
 // merge File list from /Application and /opt/... before comparing
 gatherLocalApps.done(function() {
   allLocalFiles = _.merge(optFiles, appFiles);
 });
+
 
 // get list of files from github object
 getGithubFiles.then(function(res) {
@@ -53,10 +56,10 @@ getGithubFiles.then(function(res) {
   _.forEach(files, function(val, i) {
     caskFiles[i] = removeExtension(val);
   });
-  return caskFiles;
 }).fin();
 
 
+// find common files and feed them to file-template
 getCommonFiles
   .then(function() {
     commonFiles = _.intersection(allLocalFiles, caskFiles).sort();
