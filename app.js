de095511093786c55a382d17f4b66f65487ab95b
Adam L <github@adamin.net>
2013-12-15 18:16:51 -0800

    Moved parse-cask out of app

--- app.js
+++ app.js
@@ -4,32 +4,18 @@ var _               = require('lodash'),
     FS              = require('q-io/fs'),
     getArgv         = require('./helpers/parse-arg'),
     getLocalFiles   = require('./helpers/get-local-files'),
-    brewTpl         = require('./templates/brew-template'),
-    caskFileTpl     = require('./templates/cask-file-template'),
     startBrewFiles  = require('./helpers/parse-brew-files'),
+    startCaskFiles  = require('./helpers/parse-cask-files'),
+    brewTpl         = require('./templates/brew-template'),
 
     // paths
     brewPath        = '/usr/local/Cellar/',
-    optPath         = '/opt/homebrew-cask/Caskroom/',
-    appPath         = '/Applications',
     caskPath        = '/usr/local/Library/Taps/phinze-cask/Casks/'
     installAppDir   = getArgv(process.argv),
 
-    // instantiate arrays
-    optFiles        = [],
-    appFiles        = [],
-    allLocalCasks   = [],
-    caskFiles       = [],
-    commonCasks     = [];
-
-
-// promises
-var getLocalBrew    = FS.exists(brewPath),
-    getOptFiles     = getLocalFiles(optPath),
-    getAppFiles     = getLocalFiles(appPath),
-    getCaskList     = getLocalFiles(caskPath),
-    gatherLocalApps = Q.all([getOptFiles, getAppFiles]),
-    getCommonCasks  = Q.all([getCaskList, gatherLocalApps]),
+    // promises
+    getLocalBrew    = FS.exists(brewPath),
+    getCommonCasks  = FS.exists(caskPath),
     allComplete     = Q.all([getLocalBrew, getCommonCasks]);
 
 
@@ -40,54 +26,17 @@ getLocalBrew
     callError(err);
   }).fin();
 
+getCommonCasks
+  .then(function(bool) {
+    return startCaskFiles()
+  }, function(err) {
+    callError(err);
+  }).fin();
+
 
 // write brew file
 FS.write(".brew", brewTpl()).fin();
 
-
-// get all local applications from Applications and /opt/
-getOptFiles.then(function(files) {
-  optFiles = files;
-}, function(err) {
-  callError(err);
-}).fin();
-
-getAppFiles.then(function(files) {
-  appFiles = files;
-}, function(err) {
-  callError(err);
-}).fin();
-
-
-// merge File list from /Application and /opt/... before comparing
-gatherLocalApps.done(function() {
-  allLocalCasks = _.merge(optFiles, appFiles);
-});
-
-
-// get list of all casks
-getCaskList.then(function(files) {
-  caskFiles = files;
-}, function(err) {
-  callError(err);
-}).fin();
-
-
-// find common files and feed them to file-template
-getCommonCasks
-  .then(function() {
-    commonCasks = _.intersection(allLocalCasks, caskFiles).sort();
-    return commonCasks;
-  })
-  .then(function(files) {
-    var text = caskFileTpl(files, installAppDir);
-    return text;
-  })
-  .then(function(text) {
-    FS.write(".Caskfile", text);
-  }, function(err) {
-    callError(err);
-  }).fin();
 
 allComplete.then(function() {
   console.log('Move to your home directory and simply type "brew bundle" to get started! ');
---
+++ helpers/parse-cask-files.js
@@ -0,0 +1,79 @@
+// requires
+var _               = require('lodash'),
+    Q               = require('q'),
+    FS              = require('q-io/fs'),
+    getArgv         = require('./parse-arg'),
+    getLocalFiles   = require('./get-local-files'),
+    caskFileTpl     = require('../templates/cask-file-template'),
+
+    // paths
+    optPath         = '/opt/homebrew-cask/Caskroom/',
+    appPath         = '/Applications',
+    caskPath        = '/usr/local/Library/Taps/phinze-cask/Casks/'
+    installAppDir   = getArgv(process.argv),
+
+    // instantiate arrays
+    optFiles        = [],
+    appFiles        = [],
+    allLocalCasks   = [],
+    caskFiles       = [],
+    commonCasks     = [];
+
+
+// promises
+var getOptFiles     = getLocalFiles(optPath),
+    getAppFiles     = getLocalFiles(appPath),
+    getCaskList     = getLocalFiles(caskPath),
+    gatherLocalApps = Q.all([getOptFiles, getAppFiles]),
+    getCommonCasks  = Q.all([getCaskList, gatherLocalApps]);
+
+
+// get all local applications from Applications and /opt/
+getOptFiles.then(function(files) {
+  optFiles = files;
+}, function(err) {
+  callError(err);
+}).fin();
+
+getAppFiles.then(function(files) {
+  appFiles = files;
+}, function(err) {
+  callError(err);
+}).fin();
+
+
+// merge File list from /Application and /opt/... before comparing
+gatherLocalApps.done(function() {
+  allLocalCasks = _.merge(optFiles, appFiles);
+});
+
+
+// get list of all casks
+getCaskList.then(function(files) {
+  caskFiles = files;
+}, function(err) {
+  callError(err);
+}).fin();
+
+
+// find common files and feed them to file-template
+getCommonCasks
+  .then(function() {
+    commonCasks = _.intersection(allLocalCasks, caskFiles).sort();
+    return commonCasks;
+  })
+  .then(function(files) {
+    var text = caskFileTpl(files, installAppDir);
+    return text;
+  })
+  .then(function(text) {
+    FS.write(".Caskfile", text);
+  }, function(err) {
+    callError(err);
+  }).fin();
+
+
+// return promise
+module.exports = function (args) {
+  return getCommonCasks;
+};
