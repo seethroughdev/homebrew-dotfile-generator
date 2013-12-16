8a1cc6f93055a043ba7d795a4d11f1c65aba8ff3
Adam L <github@adamin.net>
2013-12-15 18:30:16 -0800

    Improved promise return

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var _               = require('lodash'),
     Q               = require('q'),
     FS              = require('q-io/fs'),
     getArgv         = require('./helpers/parse-arg'),
-    getLocalFiles   = require('./helpers/get-local-files'),
     startBrewFiles  = require('./helpers/parse-brew-files'),
     startCaskFiles  = require('./helpers/parse-cask-files'),
     brewTpl         = require('./templates/brew-template'),
@@ -14,34 +13,29 @@ var _               = require('lodash'),
     installAppDir   = getArgv(process.argv),
 
     // promises
-    getLocalBrew    = FS.exists(brewPath),
-    getCommonCasks  = FS.exists(caskPath),
-    allComplete     = Q.all([getLocalBrew, getCommonCasks]);
+    getLocalBrew    = startBrewFiles(),
+    getCommonCasks  = startCaskFiles(),
+    writeBrew       = FS.write(".brew", brewTpl()),
+    allComplete     = Q.all([getLocalBrew, getCommonCasks, writeBrew]);
 
 
+// complete promises
 getLocalBrew
-  .then(function(bool) {
-    return startBrewFiles()
-  }, function(err) {
-    callError(err);
+  .fail(function(err) {
+    console.log('ERROR: Brewfile was not written!', err);
   }).fin();
 
 getCommonCasks
-  .then(function(bool) {
-    return startCaskFiles()
-  }, function(err) {
-    callError(err);
+  .fail(function(err) {
+    console.log('ERROR: Caskfile was not written!', err);
   }).fin();
 
+writeBrew
+  .fail(function(err) {
+    console.log('ERROR: .brew file was not written!', err);
+  }).fin();
 
-// write brew file
-FS.write(".brew", brewTpl()).fin();
-
-
+// onComplete
 allComplete.then(function() {
   console.log('Move to your home directory and simply type "brew bundle" to get started! ');
 }).fin();
-
-function callError(err) {
-  console.error('ERROR: ' + err);
-}
